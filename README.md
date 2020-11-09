9800e1e1ed1d98d47b4a7344f2d5bbfe61f1a7b8
jimmy_ko <jimmy_ko@papy.com.tw>
2020-11-09 16:46:47 +0800

    1109

---
+++ README.md
@@ -0,0 +1,77 @@
+# GraphQL (use Node.js + Express)
+
+## 測試用
+
+### Packages
+
+1. express
+2. graphql
+3. express-graphql
+4. sqlite3 (讀取本地 Database)
+5. cors (處理 CORS)
+
+### 步驟
+
+1. `git clone ssh://git@m.papy.com.tw:10022/jimmy_ko/graphql-server.git`
+2. `cd graphql-server`
+3. `npm install`
+4. `node index.js`
+5. `http://localhost:4000/graphql`
+
+### Query books all list
+
+```
+query {
+  booksAll {
+    sid
+    name
+    author
+    cover
+  }
+}
+```
+
+### Query books page 1 / limit 20
+
+```
+query {
+  booksPage(page: 1) {
+    sid
+    name
+    author
+    cover
+  }
+}
+```
+
+### Query sid
+
+```
+query {
+  book(sid: 74171) {
+    sid
+    name
+    author
+    cover
+  }
+}
+```
+
+### POST
+
+```
+method: post
+url: http://localhost:4000/graphql
+data:
+  query {
+    book(sid: 74171) {
+      sid
+      name
+      author
+      cover
+    }
+  }
+
+```
+
+https://blog.jscrambler.com/build-a-graphql-api-with-node/
---
+++ index.js
@@ -0,0 +1,124 @@
+const express = require('express');
+const cors = require('cors');
+const graphql = require('graphql');
+const sqlite3 = require('sqlite3').verbose();
+const { graphqlHTTP } = require('express-graphql');
+
+const app = express();
+const database = new sqlite3.Database('./db.sqlite');
+
+/* ----------------------------------------------------------------------------------------------
+   GraphQL
+*/
+const BookType = new graphql.GraphQLObjectType({
+  name: 'Book',
+  fields: {
+    sid: { type: graphql.GraphQLID },
+    name: { type: graphql.GraphQLString },
+    author: { type: graphql.GraphQLString },
+    category_id: { type: graphql.GraphQLInt },
+    category_name: { type: graphql.GraphQLString },
+    cover: { type: graphql.GraphQLString },
+  },
+});
+
+var queryType = new graphql.GraphQLObjectType({
+  name: 'Query',
+  fields: {
+    booksAll: {
+      type: graphql.GraphQLList(BookType),
+      resolve: (root, args, context, info) => {
+        return new Promise((resolve, reject) => {
+          database.all(
+            'SELECT * FROM books ORDER BY start DESC LIMIT 500;',
+            function (err, rows) {
+              if (err) {
+                reject([]);
+              }
+              resolve(rows);
+            }
+          );
+        });
+      },
+    },
+    booksPage: {
+      type: graphql.GraphQLList(BookType),
+      args: {
+        page: {
+          type: new graphql.GraphQLNonNull(graphql.GraphQLInt),
+        },
+      },
+      resolve: (root, { page }, context, info) => {
+        let offsetCount = (page - 1) * 20;
+        return new Promise((resolve, reject) => {
+          database.all(
+            'SELECT * FROM books ORDER BY start DESC limit 20 offset (?);',
+            [offsetCount],
+            function (err, rows) {
+              if (err) {
+                reject([]);
+              }
+              resolve(rows);
+            }
+          );
+        });
+      },
+    },
+    book: {
+      type: BookType,
+      args: {
+        sid: {
+          type: new graphql.GraphQLNonNull(graphql.GraphQLID),
+        },
+      },
+      resolve: (root, { sid }, context, info) => {
+        return new Promise((resolve, reject) => {
+          database.all('SELECT * FROM books WHERE sid = (?);', [sid], function (
+            err,
+            rows
+          ) {
+            if (err) {
+              reject(null);
+            }
+            resolve(rows[0]);
+          });
+        });
+      },
+    },
+  },
+});
+
+const schema = new graphql.GraphQLSchema({
+  query: queryType,
+});
+
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema: schema,
+    graphiql: true,
+  })
+);
+
+/* ----------------------------------------------------------------------------------------------
+   CORS
+*/
+const corsOptions = {
+  origin: [
+    'http://www.example.com',
+    'http://192.168.66.165:8000',
+    'http://localhost:8000',
+    'http://localhost',
+
+    'https://gatsby.kyjhome.com/',
+    'http://gatsby.kyjhome.com/',
+  ],
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+  allowedHeaders: ['Content-Type', 'Authorization'],
+};
+app.use(cors(corsOptions));
+
+/* ----------------------------------------------------------------------------------------------
+   Listen
+*/
+app.listen(4000);
